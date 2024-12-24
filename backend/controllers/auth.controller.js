c0172b87f55672d8f6e87dc25df6ef0bf7ff0ab3
saagarbisht <sgrcoc3@gmail.com>
2024-12-24 12:01:25 +0530

    login(invalid password, email) isuue solve

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -77,7 +77,7 @@ export const login = async (req, res) => {
         role: user.role,
       });
     } else {
-      return res.status(401).json({ message: "Invalid email or password" });
+      return res.status(400).json({ message: "Invalid email or password" });
     }
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
